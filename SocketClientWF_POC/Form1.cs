584449934fee9d6f03e99eea0f9dbf6953e05b54
Javier Becerril <javier.becerril@elipgo.com>
2023-09-16 16:37:05 -0600

    cambios en Client WinForms

--- SocketClientWF_POC/Form1.cs
+++ SocketClientWF_POC/Form1.cs
@@ -11,28 +11,10 @@ namespace SocketClientWF_POC
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+            
         }
 
-        private static void Ws_OnMessage(object sender, MessageEventArgs e)
-        {
-            MessageBox.Show("Received from the server: " + e.Data);
-
-            try
-            {
-                //Vector pos = JsonConvert.DeserializeObject<Vector>(e.Data);
-                //Console.WriteLine("Created a vector: " + pos.x + "," + pos.y);
-                //DrawDot(pos.x, pos.y, 50, 15, 1);
-            }
-            catch (Exception ex)
-            {
-                //Console.WriteLine(ex);
-                //Console.WriteLine("I don't know what to do with \"" + e.Data + "\"");
-                MessageBox.Show("I don't know what to do with \"" + e.Data + "\"");
-            }
-
-        }
-
+        
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -40,6 +22,32 @@ namespace SocketClientWF_POC
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Create a scoped instance of a WS client that will be properly disposed
+            //using (WebSocket ws = new WebSocket("ws://simple-websocket-server-echo.glitch.me/"))
+            using (WebSocket ws = new WebSocket("ws://127.0.0.1:7890/EchoAll"))
+            {
+                ws.OnMessage += Ws_OnMessage1;
+                ws.Connect();
+                ws.Send("Hello from Client!");
+                //Console.ReadKey();
+            }
+        }
+
+        private void Ws_OnMessage1(object? sender, MessageEventArgs e)
+        {
+            try
+            {
+                //Vector pos = JsonConvert.DeserializeObject<Vector>(e.Data);
+                //Console.WriteLine("Created a vector: " + pos.x + "," + pos.y);
+                //DrawDot(pos.x, pos.y, 50, 15, 1);
+                MessageBox.Show("Received from the server: " + e.Data);
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex);
+                //Console.WriteLine("I don't know what to do with \"" + e.Data + "\"");
+                MessageBox.Show("I don't know what to do with \"" + e.Data + "\"");
+            }
 
         }
     }
