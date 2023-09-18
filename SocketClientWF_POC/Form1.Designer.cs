c33ed5b99de77c2ae0721fd3f549d5661979eff1
Javier Becerril <javier.becerril@elipgo.com>
2023-09-17 22:36:30 -0600

    Se reciben broadcast del server

--- SocketClientWF_POC/Form1.Designer.cs
+++ SocketClientWF_POC/Form1.Designer.cs
@@ -30,41 +30,55 @@
         {
             listBox1 = new ListBox();
             button1 = new Button();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(12, 12);
+            listBox1.Location = new Point(12, 44);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(351, 199);
+            listBox1.Size = new Size(351, 169);
             listBox1.TabIndex = 2;
             // 
             // button1
             // 
-            button1.Location = new Point(382, 12);
+            button1.Location = new Point(288, 11);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 3;
-            button1.Text = "button1";
+            button1.Text = "Send";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(270, 23);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "Inicio";
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(469, 225);
+            ClientSize = new Size(372, 225);
+            Controls.Add(textBox1);
             Controls.Add(button1);
             Controls.Add(listBox1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private ListBox listBox1;
         private Button button1;
+        private TextBox textBox1;
     }
 }
--- SocketClientWF_POC/Form1.cs
+++ SocketClientWF_POC/Form1.cs
@@ -9,34 +9,78 @@ namespace SocketClientWF_POC
             InitializeComponent();
         }
 
-
-
-        private void Ws_OnMessage(object? sender, MessageEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            try
+            using (WebSocket ws = new WebSocket("ws://127.0.0.1:7890/EchoAll"))
             {
-                MessageBox.Show("Received from the server: " + e.Data);
-                listBox1.Items.Add("Echo: " + e.Data);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("I don't know what to do with \"" + e.Data + "\"");
+                ws.OnOpen += (sender, e) =>
+                 listBox1.Items.Add(string.Format("Connected to {0} successfully ", "ws://127.0.0.1:7890/EchoAll"));
+                ws.OnError += (sender, e) =>
+                   listBox1.Items.Add("Error: " + e.Message);
+                ws.OnMessage += (sender, e) =>
+                   listBox1.Items.Add("Echo: " + e.Data);
+                ws.OnClose += (sender, e) =>
+                   listBox1.Items.Add(string.Format("Disconnected with {0}", "ws://127.0.0.1:7890/EchoAll"));
+                ws.OnMessage += Ws_OnMessage;
+                ws.Connect();
+                ws.Send("Hello from Windows Form Client!");
+                Thread.Sleep(1000);
             }
         }
 
+        private static void Ws_OnMessage(object? sender, MessageEventArgs e)
+        {
+            try
+            {
+                CommonData.serverData = e.Data;
+                //MessageBox.Show("Received from the server: " + e.Data);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("I don't know what to do with \"" + e.Data + "\"");
+            }
+        }
+
+        private  void Ws_OnMessage1(object? sender, MessageEventArgs e)
+        {
+            try
+            {
+                CommonData.serverData = e.Data;
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("I don't know what to do with \"" + e.Data + "\"");
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             using (WebSocket ws = new WebSocket("ws://127.0.0.1:7890/EchoAll"))
             {
-                //ws.OnOpen += (ss, ee) =>
-                // listBox1.Items.Add(string.Format("Connected to {0} successfully ", "ws://127.0.0.1:7890/EchoAll"));
-                //ws.OnError += (ss, ee) =>
-                //   listBox1.Items.Add("    Error: " + ee.Message);
-                //ws.OnClose += (ss, ee) =>
-                //   listBox1.Items.Add(string.Format("Disconnected with {0}", "ws://127.0.0.1:7890/EchoAll"));
+
                 ws.OnMessage += Ws_OnMessage;
                 ws.Connect();
-                ws.Send("Hello from Windows Client!");
+                ws.Send("Hello from Windows Button Client!");
+                string received = string.IsNullOrEmpty(CommonData.serverData) ? "Error" : CommonData.serverData;
+                listBox1.Items.Add(received);
+                textBox1.Text = "";
+                textBox1.Text = received;
+                //Thread.Sleep(500);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            using (WebSocket ws = new WebSocket("ws://127.0.0.1:7890/EchoAll"))
+            {
+
+                ws.OnMessage += Ws_OnMessage1;
+                ws.Connect();
+                ws.Send("Hello from Windows Text Client!");
+                string received = string.IsNullOrEmpty(CommonData.serverData) ? "Error" : CommonData.serverData;
+                listBox1.Items.Add(received);
+                textBox1.Text = received;
+                Thread.Sleep(500);
             }
         }
     }
